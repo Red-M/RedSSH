02306ee76c24f25718649bec759671b7a37666f1
Red_M <git@red-m.net>
2019-01-12 22:46:59 +1000

    Add license and fix some line endings.

--- redssh/__init__.py
+++ redssh/__init__.py
@@ -1,1 +1,24 @@
-"""An SSH automation library... moduleauthor:: Red_M <redssh_docs@red-m.net>"""from . import redsshRedSSH = redssh.RedSSH
+# RedSSH
+# Copyright (C) 2018  Red_M ( http://bitbucket.com/Red_M )
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""An SSH automation library.
+
+.. moduleauthor:: Red_M <redssh_docs@red-m.net>
+
+"""
+from . import redssh
+
+RedSSH = redssh.RedSSH
