526aa48ec5dbb4e55d4bea4f6561e147d3c40322
Red_M <git@red-m.net>
2019-02-11 18:50:00 +1000

    Update docs and add a device_init().

--- redssh/redssh.py
+++ redssh/redssh.py
@@ -33,7 +33,7 @@ class RedSSH(object):
     '''
     Instances the start of an SSH connection.
     Extra options are available at :func:`redssh.RedSSH.connect` time.
-    
+
     :param ssh_key_policy: `paramiko`'s policy for handling server SSH keys. Defaults to `paramiko.client.RejectPolicy`
     :type ssh_key_policy: `paramiko.client.SSHKeyPolicy`
     :param prompt: The basic prmopt to expect for the first command line.
@@ -55,9 +55,10 @@ class RedSSH(object):
         else:
             self.set_ssh_key_policy(ssh_key_policy)
         self.quit = self.exit
-    
+
     def __pexpect_and_paramiko_expect_bind__(self):
         '''
+        This is an internal binder to make use of the pexpect function names for paramiko_expect.
         '''
         self.PROMPT = self.prompt
         self.UNIQUE_PROMPT = r"\[PEXPECT\][\$\#] "
@@ -65,47 +66,53 @@ class RedSSH(object):
         self.PROMPT_SET_CSH = r" set prompt='[PEXPECT]\$ '"
         self.expect = self.screen.expect
         self.sendline = self.screen.send
-    
+
     def __check_for_attr__(self,attr):
-        '''
-        '''
         return(attr in self.__dict__)
-    
+
     def set_ssh_key_policy(self,ssh_key_policy):
         '''
         Just a shortcut for `paramiko.client.set_missing_host_key_policy`
-        
+
         :param ssh_key_policy: `paramiko`'s policy for handling server SSH keys. Defaults to `paramiko.client.RejectPolicy`
         :type ssh_key_policy: `paramiko.client.SSHKeyPolicy`
         '''
         self.client.set_missing_host_key_policy(ssh_key_policy)
-    
+
     def connect(self,**kwargs):
         '''
         All options for this is located in :py:meth:`paramiko.client.SSHClient.connect`
         '''
         self.client.connect(**kwargs)
         self.screen = paramiko_expect.SSHClientInteraction(self.client, tty_width=0, tty_height=0, display=self.debug)
+        self.__pexpect_and_paramiko_expect_bind__() # do this early to make sure that users can call expected functions in the device_init.
+        self.device_init()
         self.screen.expect(self.prompt)
         self.past_login = True
-        self.__pexpect_and_paramiko_expect_bind__()
         self.set_unique_prompt()
-    
+
+    def device_init(**kwargs):
+        '''
+        Override this function to intialize a device that does not simply drop to the terminal or that will kick you out if you send any key other than an "accpetable" one.
+        This default one will work on linux quite well but devices such as pfsense or mikrotik might require this function and :func:`redssh.RedSSH.get_unique_prompt` to be overriden.
+        '''
+        pass
+
     def get_unique_prompt(self):
         '''
-        Return a unique prompt from the existing SSH session.
-        
+        Return a unique prompt from the existing SSH session. Override this function to generate the compiled regex however you'd like, eg, from a database or from a hostname.
+
         :returns: compiled `rstring`
         '''
         return(re.escape(self.command('',raw=True)[1:])) # A smart-ish way to get the current prompt after a dumb prompt match
-    
+
     def set_unique_prompt(self,use_basic_prompt=True,set_prompt=False):
         '''
         Set a unique prompt in the existing SSH session.
-        
+
         :param use_basic_prompt: Use the dumb prompt from first login to the remote terminal.
         :type use_basic_prompt: `bool`
-        :param set_prompt: Set to `True` to set the prompt via `self.PROMPT_SET_SH`
+        :param set_prompt: Set to `True` to set the prompt via `redssh.RedSSH.PROMPT_SET_SH`
         :type set_prompt: `bool`
         '''
         if use_basic_prompt==True:
@@ -113,39 +120,39 @@ class RedSSH(object):
         if set_prompt==True:
             self.command(self.PROMPT_SET_SH)
         self.prompt = self.get_unique_prompt()
-    
+
     def command(self,cmd,raw=False,prompt_change=False,reset_prompt=False):
         '''
         Run a command in the remote terminal.
-        
+
         :param cmd: Command to execute, I'd treat this like I am typing into bash myself.
         :type cmd: `str`
         :param raw: Set to `True` to remove the "smart" cleaning, useful for debugging or for when you want the prompt as well.
         :type raw: `bool`
         :param prompt_change: Set to `True` when the command executed changes the prompt to expect for when the command finishes, so that the prompt value is automatically set for you.
         :type prompt_change: `bool`
-        :param reset_prompt: Set to `True` to allow when `prompt_change` is set to `True` to set the prompt via `self.PROMPT_SET_SH`.
+        :param reset_prompt: Set to `True` to allow when `prompt_change` is set to `True` to set the prompt via :attr:`redssh.RedSSH.PROMPT_SET_SH`.
         :type reset_prompt: `bool`
         '''
         self.sendline(cmd)
-        
+
         if prompt_change==True:
             self.expect(self.basic_prompt)
             self.set_unique_prompt(set_prompt=reset_prompt)
         elif prompt_change==False:
             self.expect(self.prompt)
-        
+
         if raw==False:
             out = self.screen.current_output_clean[:-1] # always adds a new line for whatever reason
         elif raw==True:
             out = self.screen.current_output
-        
+
         return(out)
-    
+
     def sudo(self,password,sudo=True,su_cmd='su -'):
         '''
         Sudo up or SU up or whatever up into higher priviledges.
-        
+
         :param password: Password for gaining priviledges
         :type password: `str`
         :param sudo: Set to `False` to allow `su_cmd` to be executed instead.
@@ -161,24 +168,24 @@ class RedSSH(object):
         self.sendline(password)
         self.expect(self.basic_prompt)
         self.set_unique_prompt()
-        
-    
+
+
     def start_scp(self):
         '''
         Start the SFTP client.
         '''
         if not self.__check_for_attr__('sftp_client'):
             self.sftp_client = self.client.open_sftp()
-    
+
     def put_folder(self,local_path,remote_path,recursive=False):
         '''
         Upload an entire folder via SFTP to the remote session. Similar to `cp /files/* /target`
         Also retains file permissions.
-        
+
         .. warning::
-        
+
         Do note that this will only upload with the user you logged in as, not the current user you are running commands as.
-        
+
         :param local_path: The local path, on the machine where your code is running from, to upload from.
         :type local_path: `str`
         :param remote_path: The remote path to upload the `local_path` to.
@@ -200,16 +207,16 @@ class RedSSH(object):
                     remote_file_path = os.path.join(remote_path, filename)
                     if os.path.sep.join(local_file_path.split(os.path.sep)[:-1])==local_path:
                         self.put_file(local_file_path,remote_file_path)
-    
+
     def put_file(self,local_path,remote_path):
         '''
         Upload file via SFTP to the remote session. Similar to `cp /files/file /target`.
         Also retains file permissions.
-        
+
         .. warning::
-        
+
         Do note that this will only upload with the user you logged in as, not the current user you are running commands as.
-        
+
         :param local_path: The local path, on the machine where your code is running from, to upload from.
         :type local_path: `str`
         :param remote_path: The remote path to upload the `local_path` to.
@@ -218,7 +225,7 @@ class RedSSH(object):
         if self.__check_for_attr__('sftp_client'):
             self.sftp_client.put(local_path,remote_path)
             self.sftp_client.chmod(remote_path,os.stat(local_path).st_mode)
-    
+
     def exit(self):
         '''
         Kill the current session if actually connected.
@@ -227,4 +234,4 @@ class RedSSH(object):
         if self.__check_for_attr__('past_login'):
             if self.past_login==True:
                 self.client.close()
-        
+
