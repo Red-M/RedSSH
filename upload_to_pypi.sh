cf5a157a92c83f7c9fc7bb13101095c7550937e8
Red_M <git@red-m.net>
2019-03-23 14:58:38 +1000

    Update pypi upload script and gitignore.

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+mkdir ./tmp
+cp -r ./* ./tmp/
+cd ./tmp
+\rm -rf ./redssh.egg-info ./tmp ./dist
 python3 -m pip install --user --upgrade setuptools wheel
 python3 -m pip install --user --upgrade twine
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
+cd ../
+\rm -rf ./tmp
