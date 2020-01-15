65ed39aa40eb64ca1ddc35f78ff5a16608fa2bb0
Red_M <git@red-m.net>
2020-01-15 22:27:51 +1000

    Fix minor bug in CICD related to coveralls.

--- tests/scripts/test.sh
+++ tests/scripts/test.sh
@@ -4,6 +4,7 @@ CI_SYSTEM=${1}
 PYTHON_VERSION=${2}
 
 if [ ! -z $CI_SYSTEM ] && [ ${CI_SYSTEM} != "LOCAL" ]; then
+    git checkout master
     eval "$(ssh-agent \-s)"
     chmod 600 ./tests/ssh_host_key
     ssh-add ./tests/ssh_host_key
