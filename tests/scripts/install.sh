8fd868a462f3e74e905502505896fa2ff8693419
Red_M <git@red-m.net>
2020-12-20 21:56:37 +1000

    Remove using redlibssh from builds and automated testing.

--- tests/scripts/install.sh
+++ tests/scripts/install.sh
@@ -6,11 +6,11 @@ PYTHON_MINOR_VERSION=${3}
 pip${PYTHON_VERSION} install -e ./[tests]
 pip${PYTHON_VERSION} install -e ./[docs]
 
-mkdir ./build
-cd ./build
-git clone https://github.com/Red-M/redlibssh2.git || true
-cd ./redlibssh2
-git submodule update --init --recursive || true
-pip${PYTHON_VERSION} uninstall -y ssh2-python
-pip${PYTHON_VERSION} install -e ./
-cd ../../
+# mkdir ./build
+# cd ./build
+# git clone https://github.com/Red-M/redlibssh2.git || true
+# cd ./redlibssh2
+# git submodule update --init --recursive || true
+# pip${PYTHON_VERSION} uninstall -y ssh2-python
+# pip${PYTHON_VERSION} install -e ./
+# cd ../../
