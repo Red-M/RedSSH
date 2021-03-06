54b0621f922730ba63a8a019a6fd014d45b840a0
Red_M <git@red-m.net>
2021-03-06 19:20:09 +1000

    Make sure we're not making a franken-debian with local mirrors.

--- tests/scripts/install_root.sh
+++ tests/scripts/install_root.sh
@@ -4,10 +4,11 @@ PYTHON_MAJOR_VERSION=${2}
 PYTHON_MINOR_VERSION=${3}
 
 if [ -n $CI_SYSTEM ] && [ ${CI_SYSTEM} == "GITLAB" ] && [ ! -z $(getent hosts deb-mirror | awk '{ print $1 }') ]; then
-    echo "deb http://deb-mirror/deb.debian.org/debian stable main non-free contrib
-deb-src http://deb-mirror/deb.debian.org/debian stable main non-free contrib
-deb http://deb-mirror/deb.debian.org/debian stable-updates main contrib non-free
-deb-src http://deb-mirror/deb.debian.org/debian stable-updates main contrib non-free
+    DEBIAN_VERSION_CODENAME=$(grep VERSION_CODENAME /etc/os-release | sed -E 's/.+?\=//g')
+    echo "deb http://deb-mirror/deb.debian.org/debian ${DEBIAN_VERSION_CODENAME} main non-free contrib
+deb-src http://deb-mirror/deb.debian.org/debian ${DEBIAN_VERSION_CODENAME} main non-free contrib
+deb http://deb-mirror/deb.debian.org/debian ${DEBIAN_VERSION_CODENAME}-updates main contrib non-free
+deb-src http://deb-mirror/deb.debian.org/debian ${DEBIAN_VERSION_CODENAME}-updates main contrib non-free
 $(cat /etc/apt/sources.list)" > /etc/apt/sources.list
 fi
 
